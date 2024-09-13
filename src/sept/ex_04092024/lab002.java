c3bb08022ba1b2b065f3feeb7bcf1e9807ce8610
yasmeennabinaikar <zaarakhan1234saniya@gmail.com>
2024-09-13 20:16:44 +0530

    resolved the issue sucessufully

--- src/sept/ex_04092024/lab002.java
+++ src/sept/ex_04092024/lab002.java
@@ -1,4 +1,12 @@
 package sept.ex_04092024;
 
-public class lab002 {
+public class lab002
+
+
+{
+    public static void main(String[] args) {
+        //System.out.println("allahamdulliah");
+        System.out.println("now im able to write the code");
+        System.out.println("idea vim was enabled");
+    }
 }
--- src/sept/ex_05092024/task01.java
+++
@@ -1,4 +0,0 @@
-package sept.ex_05092024;
-
-public class task01 {
-}
